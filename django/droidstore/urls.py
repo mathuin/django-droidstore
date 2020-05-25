baf4d6f58f68d4a1dcfa48a9c1bf9551de4fa135
Jack Twilley <mathuin@gmail.com>
2020-05-25 10:43:39 -0700

    Add support for favicon and robots

--- django/droidstore/urls.py
+++ django/droidstore/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +10,7 @@ admin.autodiscover()
 urlpatterns = [
     path('', include('store.urls', namespace='store')),
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name='robots')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
