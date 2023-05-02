2abb52b5c82547b860ebe35e69ca8447d7e124c6
Jack Twilley <mathuin@users.noreply.github.com>
2023-05-01 18:59:36 -0700

    Update to Django 4.2 and others (#27)

--- dev-setup.sh
+++ dev-setup.sh
@@ -7,10 +7,13 @@ docker-compose down -v
 # rebuild containers
 docker-compose build
 
+# momentarily spin up web to create containers
+docker-compose run --rm --no-deps --entrypoint=/bin/true web true
+
 # populate public directory and nginx directory and development SSL directory
-docker run --rm -it -v djangodroidstore_public:/public -v $(pwd):/backup busybox tar -C /public -xf /backup/public.tar
-docker run --rm -it -v djangodroidstore_nginx:/nginx -v $(pwd):/backup busybox cp /backup/nginx/droidstore.conf /nginx
-docker run --rm -it -v djangodroidstore_devssl:/devssl -v $(pwd):/backup busybox tar -C /devssl -xf /backup/devssl.tar
+docker run --rm -it -v django-droidstore_public:/public -v $(pwd):/backup busybox tar -C /public -xf /backup/public.tar
+docker run --rm -it -v django-droidstore_nginx:/nginx -v $(pwd):/backup busybox cp /backup/nginx/droidstore.conf /nginx
+docker run --rm -it -v django-droidstore_devssl:/devssl -v $(pwd):/backup busybox tar -C /devssl -xf /backup/devssl.tar
 
 # collectstatic
 docker-compose run --rm web python manage.py collectstatic --noinput
@@ -20,7 +23,7 @@ docker-compose run --rm web python manage.py migrate # --fake-initial
 
 # output logs if error
 if [[ $? == 1 ]]; then
-  docker logs djangodroidstore_db_1
+  docker logs django-droidstore-db-1
 fi
 
 # load fixtures
